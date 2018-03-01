eb93dd3623a3b8af0a2bd67077edbf9378a616b2
Alexander Lam <alam@proteus.com>
2018-02-28 19:34:21 -0800

    prev/next added and pagination events added.

--- Deliverables/index.js
+++ Deliverables/index.js
@@ -1,5 +1,6 @@
 var filteredData = dataSet;
-var pageLimit = 50;
+var pageLimit = 10;
+var currentPage = 1;
 populateTable();
 createTableNav();
 
@@ -13,11 +14,15 @@ function filterSightings(){
     clearTable();
     applyFilters();
     populateTable();
+    clearTableNav();
+    createTableNav();
 }
 
 // Filters
 function applyFilters(){
     filteredData = dataSet.filter(matchCity).filter(matchState).filter(matchCountry).filter(minDate).filter(maxDate);
+    // reset current page when changing filters
+    currentPage = 1;
 }
 
 function matchCity(sighting){
@@ -61,11 +66,15 @@ function clearTable(){
     $tbody.innerHTML='';
 }
 
-function populateTable(){
+function populateTable(pageNumber){
+    if(pageNumber === undefined){pageNumber = 1;}
+    console.log('Current PageNumber: ' + pageNumber);
     var $tbody = document.querySelector('tbody');
-    for(i = 0; i < Math.min(pageLimit, filteredData.length); i++){
+    var rows = calculatePageBounds(pageNumber);
+    console.log('Populating from ' + rows[0] + ' to ' + rows[rows.length-1])
+    for(i = rows[0]; i < Math.min(rows[rows.length-1], filteredData.length); i++){
         var sighting = filteredData[i];
-        var $row = $tbody.insertRow(i);
+        var $row = $tbody.insertRow(i-rows[0]);
         for(j = 0; j < Object.keys(sighting).length; j++){
             var $cell = $row.insertCell(j);
             $cell.innerText = sighting[Object.keys(sighting)[j]];
@@ -74,15 +83,30 @@ function populateTable(){
 }
 
 // Pagination
-function createTableNav(currentPage){
-    if(currentPage === undefined){
-        currentPage = 1;
-    }
+function createTableNav(){
+    // if(currentPage === undefined){
+    //     currentPage = 1;
+    // }
     var nPages = Math.ceil(filteredData.length/pageLimit);
     createPage(null,'previous');
-    if(nPages === 1){createPage(1);}
+    // if(nPages === 1){createPage(1);}
+    if(nPages <= 6){
+        for(n = 1; n <= nPages; n++){
+            if(currentPage === n){
+                createPage(n,'current');
+            }
+            else{createPage(n)}
+        }
+    }
     else{
-        for(n = 1; n <= Math.min(6, nPages); n++){
+        for(n = 1; n <= Math.min(3, nPages); n++){
+            if(currentPage === n){
+                createPage(n,'current');
+            }
+            else{createPage(n)}
+        }
+        createPage('...');
+        for(n = nPages-2; n <= nPages; n++){
             if(currentPage === n){
                 createPage(n,'current');
             }
@@ -90,51 +114,76 @@ function createTableNav(currentPage){
         }
     }
     createPage(null,'next');
-    if(currentPage === 1){document.getElementById('previous').class += ' disabled';}
-    if(currentPage === nPages){document.getElementById('next').class += ' disabled';}
+    // disable 'previous' or 'next' buttons if on first or last page
+    if(currentPage === 1){
+        appendAttribute(document.getElementById('previous'),'class','disabled');
+    }
+    if(currentPage === nPages){
+        appendAttribute(document.getElementById('next'),'class','disabled');
+    }
+}
+
+function clearTableNav(){
+    var $tableNav = document.getElementById('tableNav');
+    $tableNav.innerHTML = '';
 }
 
 function createPage(value,special){
     var specialValues = {
-        'previous' : '&laquo;',
-        'next' : '&raquo;'
+        'previous' : {'text' : '&laquo;', 'target': currentPage-1},
+        'next' : {'text' : '&raquo;', 'target': currentPage+1}
     }
-    
     var $tableNav = document.getElementById('tableNav');
     var $pageButton = document.createElement('li');
-    $pageButton.class = 'page-item';
+    $pageButton.setAttribute('class','page-item');
     var $pageLink = document.createElement('a');
     $pageLink.setAttribute('class','page-link');
     $pageLink.setAttribute('href','#');
     if(special === 'current'){
         $pageButton.setAttribute('id','currentPage');
-        $pageButton.class += ' active';
+        // $pageButton.class += ' active';
+        appendAttribute($pageButton,'class','active');
         $pageLink.innerText = value;
     }
     else if(special === 'previous' || special === 'next'){
         $pageLink.setAttribute('aria-label', special);
         $pageButton.setAttribute('id', special);
-        $pageLink.innerHTML = '<span aria-hidden="true">' + specialValues[special] + '</span>\
+        $pageLink.innerHTML = '<span aria-hidden="true">' + specialValues[special]['text'] + '</span>\
             <span class="sr-only">' + special + '</span>';
+        $pageLink.addEventListener('click',function(){goToPage(specialValues[special]['target']);})
     }
-    else{$pageLink.innerText = value;}
+    else{
+        $pageLink.innerText = value;
+        $pageLink.addEventListener('click',function(){goToPage(value);})
+    }
     $pageButton.appendChild($pageLink);
     $tableNav.appendChild($pageButton)
 }
 
+function goToPage(targetPage){
+    currentPage = targetPage;
+    clearTable();
+    populateTable(targetPage);
+    clearTableNav();
+    createTableNav(targetPage);
+
+}
+
+// Utilities
+function appendAttribute(variable,attribute,value){
+    var current = variable.getAttribute(attribute);
+    variable.setAttribute(attribute, current + ' ' + value);
+}
+
+function calculatePageBounds(pageNumber){
+    firstRow = (pageNumber - 1) * pageLimit;
+    lastRow = pageNumber * pageLimit;
+    return [firstRow, lastRow];
+}
+
+// not used
 function getCurrentPage(){
     var $currentPage = document.getElementById('currentPage')
     return $currentPage.value
 } 
 
-// Sample previous button
-{/* <a class="page-link" href="#" aria-label="Previous">
-<span aria-hidden="true">&laquo;</span>
-<span class="sr-only">Previous</span>
-</a> */}
-
-{/* <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-<li class="page-item"><a class="page-link" href="#">1</a></li>
-<li class="page-item"><a class="page-link" href="#">2</a></li>
-<li class="page-item"><a class="page-link" href="#">3</a></li>
-<li class="page-item"><a class="page-link" href="#">Next</a></li> */}
